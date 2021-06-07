de3d30b2b3e8a5e9e8e0c5cf621af1ab723940df
Bradley Smith <bradley.smith@thebrilliantclub.org>
2021-06-07 09:46:09 +0100

    functions folder

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,36 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require("express");
+const cors = require("cors");
+// eslint-disable-next-line max-len
+const stripe = require("stripe")("sk_test_51IyCkCGDpXfgd5Nm8stgabBNB4bOptc2b96cvfgUqIaEbz4iXB3ZbJklfhy4h01VCFfADOrsENbk43iPK0i745Yp00dDk7iiLu");
+
+// API
+
+// App config
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API routes
+app.get("/", (req, res)=> res.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total; // targets the query param in front end
+  console.log("payment request received for this amount >>>", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+  // OK -created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+// Listen command
+exports.api = functions.https.onRequest(app);
+
+// http://localhost:5001/fir-cccd0/us-central1/api
