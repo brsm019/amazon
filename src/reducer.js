41c86bc1afdd7287edecf7c17b3d236af6590c01
Bradley Smith <bradleysmith@Bs-Macbook.local>
2021-06-09 19:19:35 +0100

    searchInput branch

--- src/reducer.js
+++ src/reducer.js
@@ -2,62 +2,60 @@ export const initialState = {
   basket: [],
   user: null,
   searchProduct: "",
-}
+};
 
-export const getBasketTotal = (basket) => basket?.reduce(
-  (amount, item) => item.price + amount,
-  0
-)
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_TO_BASKET':
+    case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
-      }
+      };
+    //tests
+    case "ADD_SEARCH_INPUT":
+      let len = state.searchProduct.length;
 
-      case 'ADD_SEARCH_INPUT':
+      let newSearch = [...state.searchProduct];
 
-        let len = state.searchProduct.length
+      len > 0 && newSearch.splice(len, 1);
 
-        let newSearch = [...state.searchProduct]
+      return {
+        ...state,
+        searchProduct: [newSearch, action.inputValue],
+      };
 
-        len > 0 && newSearch.splice(len,1)
-    
-        return {
-          ...state,
-          searchProduct: [newSearch, action.inputValue]
-        }
-
-    case 'REMOVE_FROM_BASKET':
+    case "REMOVE_FROM_BASKET":
       const index = state.basket.findIndex(
         (basketItem) => basketItem.id === action.id
-      )
-      let newBasket = [...state.basket]
+      );
+      let newBasket = [...state.basket];
 
       if (index >= 0) {
-        newBasket.splice(index, 1)
+        newBasket.splice(index, 1);
       } else {
         console.warn(
           `Can't remove product (id: ${action.id}) as its not in basket!`
-        )
+        );
       }
       return {
         ...state,
         basket: newBasket,
-      }
+      };
 
-    case 'SET_USER':
+    case "SET_USER":
       return {
-        ...state, user: action.user
-      }
-    
-    case 'EMPTY_BASKET':
-      return {...state, basket: []}
+        ...state,
+        user: action.user,
+      };
+
+    case "EMPTY_BASKET":
+      return { ...state, basket: [] };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default reducer
+export default reducer;
